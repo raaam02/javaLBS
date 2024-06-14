f4ffdc7b043299a7f6397046ae661e83b400ab3e
raaam02 <kingk9869991@gmail.com>
2024-06-14 18:16:26 +0530

    Check Prime

--- javaLBS/src/javaLBS/CheckPrime.java
+++ javaLBS/src/javaLBS/CheckPrime.java
@@ -14,7 +14,7 @@ public class CheckPrime {
         if (num <= 1) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(num); i++) {
+        for (int i = 2; i <= num/2 ; i++) {
             if (num % i == 0) {
                 return false;
             }
