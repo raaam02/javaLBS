4ed5339bd08660178d6d31785b80094a62d82ab5
raaam02 <kingk9869991@gmail.com>
2024-06-14 11:08:10 +0530

    initial commit

--- javaLBS/src/javaLBS/CheckPrime.java
+++ javaLBS/src/javaLBS/CheckPrime.java
@@ -1,5 +1,25 @@
 package javaLBS;
 
 public class CheckPrime {
+    public static void main(String[] args) {
+        int number = 29; // You can change this number to test with other numbers
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
 
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
