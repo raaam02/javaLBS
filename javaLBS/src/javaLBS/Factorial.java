7eacb7458dbe99a263dea0ffa0383bf33ff3b920
raaam02 <kingk9869991@gmail.com>
2024-06-15 23:07:39 +0530

    factorial changed code

--- javaLBS/src/javaLBS/Factorial.java
+++ javaLBS/src/javaLBS/Factorial.java
@@ -1,12 +1,21 @@
 package javaLBS;
 
 public class Factorial {
+	public static int number = 8;
+	public static int fact = 1;
+	public static int getFactorial(int num) {
+		if(num == 0) {
+			return fact = 1;
+		}
+		else {
+			for(int i=1; i<=num; i++) {
+				fact *= i;
+			}
+			return fact;
+		}
+	}
 	public static void main(String[] args) {
-		 int number = 6, fact = 1;
-		 for(int i=1; i<=number; i++) {
-			 fact *= i;
-		 }
-		 System.out.println(fact);
+		 System.out.println(getFactorial(number));
 	}
 
 }
