83356fe7a3133bb15dda3f1e5c06e52861457240
raaam02 <kingk9869991@gmail.com>
2024-06-18 23:47:07 +0530

    sum of N Natural number

---
+++ javaLBS/src/javaLBS/SumOfNaturalNum.java
@@ -0,0 +1,9 @@
+package javaLBS;
+
+public class SumOfNaturalNum {
+	public static void main(String[] args) {
+		int num = 100;
+		int sumOfNNaturalNumbers = (num * (num + 1))/2;
+		System.out.println(sumOfNNaturalNumbers);
+	}
+}
