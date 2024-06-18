1b552e4828a4436105107e3885653707d630a0e6
raaam02 <kingk9869991@gmail.com>
2024-06-18 23:25:45 +0530

    find gcd of number

---
+++ javaLBS/src/javaLBS/GCD.java
@@ -0,0 +1,16 @@
+package javaLBS;
+
+public class GCD {
+	public static void main(String[] args) {
+		int num1 = 50;
+		int num2 = 75;
+		int gcd = 0;
+		
+		for (int i = 1; i <= num1 && i <= num2; i++) {
+			if (num1 % i == 0 && num2 %i == 0) {
+				gcd = i;
+			}
+		}
+		System.out.println(gcd);
+	}
+}
